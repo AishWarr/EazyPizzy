55722f36590ca6e44734d4f03040f07814ff9d5b
Aishwarya W <aishwaw@apac.corpdir.net>
2022-07-17 12:56:32 +0530

    Added DB

---
+++ EasyPizzy/ToppingType.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ToppingType]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ToppingTypeName] NCHAR(20) NULL, 
+    [ToppingTypeDescripion] NCHAR(50) NULL,
+    [IsAvailable] BIT NULL, 
+    [CreatedBy] NCHAR(30) NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [UpdatedBy] NCHAR(30) NULL, 
+    [UpdatedOn] DATETIME NULL, 
+)
---
+++ EasyPizzy/ToppingsInventoryList.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ToppingsInvetoryList]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ToppingName] NCHAR(30) NOT NULL, 
+    [ToppingDescription] NCHAR(100) NULL, 
+    [ToppingTypeId] INT NULL, 
+    [IsNonVeg] BIT NULL, 
+    [IsAvailable] BIT NULL, 
+    [ToppingCost] MONEY NULL, 
+    [CreatedDate] DATETIME NULL, 
+    [CreatedBy] NCHAR(30) NULL, 
+    [UpdatedDate] DATETIME NULL, 
+    [UpdatedBy] NCHAR(30) NULL, 
+    CONSTRAINT [FK_ToppingsInventoryList_ToppingType] FOREIGN KEY (ToppingTypeId) REFERENCES [ToppingType](Id) 
+)
